6e5fc443decd510d33d3addc3e6ead05639aaf61
Melvin Jones Repol <mrepol742@gmail.com>
2023-07-06 16:15:54 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ This is my arduino workspace where i create, test and run using Arduino IDE.
 
 ## Requirements
 - Arduino IDE
+- Arduino UNO
+
+## Libraries
 - LiquidCrystal_I2C
+- SevSeg
+- RFID
---
+++ four_seven_segment_display/four_seven_segment_display.ino
@@ -0,0 +1,18 @@
+#include "SevSeg.h"
+
+SevSeg sevseg;
+byte numDigits = 4;
+byte digitPins[] = { 10, 11, 12, 13 };
+byte segmentPins[] = { 9, 2, 3, 5, 6, 8, 7, 4 };
+bool resistorsOnSegments = true;
+byte hardwareConfig = COMMON_CATHODE;
+
+void setup() {
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  sevseg.setBrightness(90);
+}
+
+void loop() {
+  sevseg.setNumber(7421);
+  sevseg.refreshDisplay();
+}

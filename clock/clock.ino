617930741b61fb9c034ae5a1ef5bb9e46e3dc670
Melvin Jones Repol <mrepol742@gmail.com>
2023-07-14 11:03:48 +0800

    Initial Commit

---
+++ clock/clock.ino
@@ -0,0 +1,35 @@
+#include <virtuabotixRTC.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+virtuabotixRTC rtcv(6, 7, 8);
+int LED = 4;
+
+void setup()  {
+  Serial.begin(9600);
+  lcd.begin();
+  lcd.backlight();
+  // seconds, minutes, hours, day of the week, day of the month, month, year
+  rtcv.setDS1302Time(18, 10, 10, 7, 18, 7, 2023);
+  pinMode(LED, OUTPUT);
+}
+void loop()  {
+  digitalWrite(LED, HIGH);
+  rtcv.updateTime();
+  lcd.clear();
+  lcd.print(rtcv.month);
+  lcd.print("/");
+  lcd.print(rtcv.dayofmonth);
+  lcd.print("/");
+  lcd.print(rtcv.year);
+  lcd.print("  ");
+  lcd.print(rtcv.hours);
+  lcd.print(":");
+  lcd.print(rtcv.minutes);
+  lcd.print(":");
+  lcd.print(rtcv.seconds);
+  lcd.print(":");
+  digitalWrite(LED, LOW);
+  delay(500);
+}
---
+++ servor_motor_with_potentiometer/servor_motor_with_potentiometer.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+Servo Servo1;
+
+int servoPin = 8;
+int potPin = A0;
+
+
+void setup() {
+  Serial.begin(9600); 
+  Servo1.attach(servoPin);
+}
+
+void loop() {
+    int reading = analogRead(potPin);
+    Serial.println(reading);
+    int angle = map(reading, 0, 1023, 0, 180);
+    Servo1.write(angle);
+}
